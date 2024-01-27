3eb69b357f7ed546d0fcddabd17ccb22a20d045c
Raaz <21ketanraaz@gmail.com>
2024-01-28 00:17:55 +0530

    Working with Cart Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,10 @@
+import React, {useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.length
  return(
     <button className = {classes.button} onClick = {props.onClick}>
         <span className={classes.icon}>
