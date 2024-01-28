07367d1970b0c46f10ac4e4994077141155e6bb8
Raaz <21ketanraaz@gmail.com>
2024-01-28 11:00:24 +0530

    Working with refs and contexts

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,27 @@
+import {useRef, useState} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 const MealtItemForm = (props) => {
+    const amountInputRef = useRef();
+    const [amountIsValid, setamountIsValid] = useState(true);
+
+  const submitHandler = event => {
+    const enteredAmount = amountInputRef.current.value; 
+    //current is a property of useRef
+    const enteredAmountNumber = +enteredAmount;
+
+    if(enteredAmount.trim().length ===0 || enteredAmountNumber <1 || enteredAmountNumber >5){
+        setamountIsValid(false);
+        return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+            ref = {amountInputRef}
+             label="Amount" 
+             input={{
                 id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',
@@ -11,7 +29,8 @@ const MealtItemForm = (props) => {
                 step:'1',
                 defaultValue: '1'
             }}/>
-            <button>+ Add</button>
+            <button type='submit'>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount from (1-5)</p>}
         </form>
     )
 }
