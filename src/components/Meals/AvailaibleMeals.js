f9f40f904f91de1a1cb4a73095544be1156eb446
Raaz <21ketanraaz@gmail.com>
2024-02-26 11:22:06 +0530

    Handling Errors

--- src/components/Meals/AvailaibleMeals.js
+++ src/components/Meals/AvailaibleMeals.js
@@ -13,10 +13,15 @@ const AvailaibleMeals = () => {
     useEffect(()=>{
         const fetchMeals= async ()=>{
 
-        const response =await fetch('https://gymmeals-31031-default-rtdb.firebaseio.com/meals.json')
+        const response =await fetch('https://gymmeals-31031-default-rtdb.firebaseio.com/meals.json');
+
+        if(!response.ok){
+            throw new Error('Something went wrong');
+        }
             const responseData = await response.json();
 
             const loadedMeals = [];
+
             for(const key in responseData){
                 loadedMeals.push({
                     id:key,
@@ -28,13 +33,25 @@ const AvailaibleMeals = () => {
             setMeals(loadedMeals);
             setIsLoading(false);
         };
-        fetchMeals();
+          fetchMeals().catch((error)=>{
+              setIsLoading(false)
+              setHttpError(error.message);
+          });
     }, []);
 
     if(isLoading){
         return( <section className={classes.MealsLoading}>
         <p>Loading ...</p>
         </section>
+        );
+    }
+    if(httpError){
+        return(
+            <section className = {classes.MealsError}>
+                <p>
+                    {httpError}
+                </p>
+            </section>
         )
     }
     const mealsList = meals.map(meal => 
